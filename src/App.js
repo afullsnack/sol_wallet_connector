43ca54d3286e460c11655e9a5da5976edd6b362b
Miracle Friday <miraclef60@gmail.com>
2024-08-13 22:02:35 +0100

    fix: Call program Signing program transaction with connected wallet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import logo from './logo.svg';
 import { Wallet } from './components/WalletProvider';
 import './App.css';
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import React, { FC, useCallback } from "react";
 
 function App() {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const onClick = useCallback(async () => {
+    console.log(publicKey, ":::public key");
+    if (!publicKey) throw new WalletNotConnectedError();
+
+    // 890880 lamports as of 2022-09-01
+    const lamports = await connection.getMinimumBalanceForRentExemption(0);
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: Keypair.generate().publicKey,
+        lamports,
+      })
+    );
+
+    const {
+      context: { slot: minContextSlot },
+      value: { blockhash, lastValidBlockHeight }
+    } = await connection.getLatestBlockhashAndContext();
+
+    const signature = await sendTransaction(transaction, connection, { minContextSlot });
+
+    await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
+  }, [publicKey, sendTransaction, connection]);
+
+
+  // call functions
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +47,11 @@ function App() {
           Connector example with @solana/wallet-adapter
         </p>
         <Wallet></Wallet>
+        <div>
+          <button onClick={onClick}>Call send transaction</button>
+        </div>
       </header>
+
     </div>
   );
 }
